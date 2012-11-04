da780ff316c5fee4d91548dfbc48572993f5a188
Vitaly Turinsky <vitaly.turinsky@gmail.com>
2012-11-04 17:50:55 +0200

    basic socket implementation for Win

---
+++ VTTcpSocket.h
@@ -0,0 +1,31 @@
+#pragma once
+
+namespace VT
+{
+    class TcpSocketImpl;
+
+    class TcpSocket
+    {
+    public:
+
+	    TcpSocket();
+	    ~TcpSocket();
+
+	    bool connect(const char* address, const char* port, int flags = 0);
+	    bool bind(const char* address, const char* port, int flags = 0);
+
+	    bool listen();
+	    TcpSocket* accept();
+	    int receive(char *buffer, int length);
+	    int send(const char *buffer, int length);
+
+	    bool close();
+
+    private:
+        TcpSocket(const TcpSocket&);
+        TcpSocket& operator=(const TcpSocket& rhs);
+
+    private:
+        TcpSocketImpl* pimpl_;
+    };
+}
